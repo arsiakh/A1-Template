7adb84333ce4511d17272159ea1146ccdd59b194
Arsia Khorramijam <a.khorramijam@gmail.com>
2025-02-02 15:43:02 -0500

    added interface for first algorithm resolved #5

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandAlgorithm.java
@@ -0,0 +1,102 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class RightHandAlgorithm implements Explorer{ //interface explorer that will be impliemented in a righthandrule class or another algorithm class ( methods must be used the sam
+    private String[][] maze;
+
+    @Override
+    public String findPath(Maze mazeClass) {
+        maze = mazeClass.getMaze();
+        StringBuilder path = new StringBuilder();
+        String[] direction = {"NORTH", "EAST", "SOUTH", "WEST"};
+        int startRow = mazeClass.getEntryRow(); // entry row i.e (can be 4)
+        int currentCol = 0; //always start on east side so leftmost column
+        String directionValue = direction[1];
+         
+        while (!"EXIT".equals(maze[startRow][currentCol])) {
+            // Check if current position is the exit
+            //System.out.println("Current Position: (" + startRow + ", " + currentCol + "), Current Place:" + maze[startRow][currentCol] + "Direction: " + directionValue);
+            if ("EAST".equals(directionValue)) {
+                // First check right (South)
+                if ("PASS".equals(maze[startRow + 1][currentCol]) || "EXIT".equals(maze[startRow + 1][currentCol])) {
+                    directionValue = direction[2];
+                    startRow++;
+                    path.append("RF");
+                }
+                // If right is blocked, try to go straight
+                else if ("PASS".equals(maze[startRow][currentCol + 1]) || "EXIT".equals(maze[startRow][currentCol + 1])) {
+                    currentCol++;
+                    path.append("F");
+                }
+                // If straight is blocked, turn left
+                else {
+                    directionValue = direction[0];
+                    path.append("L");
+                }
+            }
+            else if ("SOUTH".equals(directionValue)) {
+                // First check right (West)
+                if ("PASS".equals(maze[startRow][currentCol - 1]) || "EXIT".equals(maze[startRow][currentCol - 1])) {
+                    directionValue=direction[3];
+                    currentCol--;
+                    path.append("RF");
+                }
+                // If right is blocked, try to go straight
+                else if ("PASS".equals(maze[startRow + 1][currentCol]) || "EXIT".equals(maze[startRow + 1][currentCol])) {
+                    startRow++;
+                    path.append("F");
+                }
+                // If straight is blocked, turn left
+                else {
+                    directionValue=direction[1];
+                    path.append("L");
+                }
+            }
+            else if ("WEST".equals(directionValue)) {
+                // First check right (North)
+                if ("PASS".equals(maze[startRow - 1][currentCol]) || "EXIT".equals(maze[startRow - 1][currentCol])) {
+                    directionValue = direction[0];
+                    startRow--;
+                    path.append("RF");
+                    
+                }
+                // If right is blocked, try to go straight
+                else if ("PASS".equals(maze[startRow][currentCol - 1]) || "EXIT".equals(maze[startRow][currentCol - 1])) {
+                    currentCol--;
+                    path.append("F");
+                    
+                }
+                // If straight is blocked, turn left
+                else {
+                    directionValue = direction[2];
+                    path.append("L");
+                }
+            }
+            else if ("NORTH".equals(directionValue)) {
+                // First check right (East)
+                if ("PASS".equals(maze[startRow][currentCol + 1]) || "EXIT".equals(maze[startRow][currentCol + 1])) {
+                    directionValue = direction[1];
+                    currentCol++;
+                    path.append("RF");
+                }
+                // If right is blocked, try to go straight
+                else if ("PASS".equals(maze[startRow - 1][currentCol]) || "EXIT".equals(maze[startRow - 1][currentCol])) {
+                    startRow--;
+                    path.append("F");
+                }
+                // If straight is blocked, turn left
+                else {
+                    directionValue = direction[3];
+                    path.append("L");
+                }
+            }
+           
+            
+            }
+            return path.toString();
+               
+        }}
+            
+       
+
+    
+
